04af6d0bbe099432bab3061854643b49d6c8d715
LukasMartinez <lukasmartinezc@gmail.com>
2022-07-16 07:26:06 -0400

    Se realiza funcion para editar los datos que estan dentro del inventario

--- main.js
+++ main.js
@@ -1,47 +1,12 @@
 let inventary = []
 let table = document.getElementById('bodyTable')
-
-function fillTable () {
-    let storedList = localStorage.getItem('inventaryData')
-    if(storedList == null) {
-        inventary = []
-    }
-    else {
-        inventary = JSON.parse(storedList)
-    }
-    table.innerHTML = ''
-    inventary.forEach((product) => {
-        table.innerHTML += `
-            <tr>
-                <th scope="row">${product.codigo}</th>
-                <td>${product.producto}</td>
-                <td>${product.precio}</td>
-                <td>${product.stock}</td>
-                <td>
-                    <button type="button" class="btn btn-primary editar">Editar</button>
-                    <button type="button" class="btn btn-danger eliminar" id="${product.codigo}">Eliminar</button>
-                </td>    
-            </tr>
-            `   
-    });
-    let deleteBtns = Array.from(document.getElementsByClassName('btn btn-danger eliminar'))
-    deleteBtns.forEach((button) => {
-        button.addEventListener('click', (event) => deleteProduct(event.target.id))
-    })
-    let editBtns = Array.from(document.getElementsByClassName('btn btn-primary editar'))
-    editBtns.forEach((button) =>{
-        button.addEventListener('click', (event) => editProduct(event.target.id))
-    })
-}
-
-fillTable()
-
 let inputCodigo = document.getElementById('codigo')
 let inputProducto = document.getElementById('producto')
 let inputPrecio = document.getElementById('precio')
 let inputStock = document.getElementById('stock')
 let btnAdd = document.getElementById('btnAdd')
 let form = document.getElementById('inventaryForm')
+let btnUpdate = document.getElementById('btnUpdate')
 
 btnAdd.addEventListener('click', (event) => addProduct(event))
 
@@ -63,7 +28,47 @@ function localStorageInventaryList (plist) {
     localStorage.setItem('inventaryData', JSON.stringify(plist))
 }
 
-localStorageInventaryList (plist) 
+function fillTable () {
+    let storedList = localStorage.getItem('inventaryData')
+    if(storedList == null) {
+        inventary = []
+    }
+    else {
+        inventary = JSON.parse(storedList)
+    }
+    table.innerHTML = ''
+    inventary.forEach((product) => {
+        table.innerHTML += `
+            <tr>
+                <th scope="row">${product.codigo}</th>
+                <td>${product.producto}</td>
+                <td>${product.precio}</td>
+                <td>${product.stock}</td>
+                <td>
+                    <button type="button" class="btn btn-primary editar" id="${product.codigo}">Editar</button>
+                    <button type="button" class="btn btn-danger eliminar" id="${product.codigo}">Eliminar</button>
+                </td>    
+            </tr>
+            `   
+    });
+    let deleteBtns = Array.from(document.getElementsByClassName('btn btn-danger eliminar'))
+    deleteBtns.forEach((button) => {
+        button.addEventListener('click', (event) => deleteProduct(event.target.id))
+    })
+    let editBtns = Array.from(document.getElementsByClassName('btn btn-primary editar'))
+    editBtns.forEach((button) =>{
+        button.addEventListener('click', (event) => { 
+            btnAdd.style.display = 'none'
+            btnUpdate.style.display = 'block'
+            editProduct(event.target.id)})
+    })
+    btnUpdate.addEventListener('click', (event) => {
+        event.preventDefault()
+        btnUpdate.style.display = 'none'
+        btnAdd.style.display = 'block'
+        updateData(event.target.getAttribute('element'))
+    })
+}
 
 function deleteProduct (codigo) {
     inventary = inventary.filter((product)=>product.codigo!==codigo)
@@ -71,9 +76,37 @@ function deleteProduct (codigo) {
     fillTable()
 }
 
-function editProduct () {
-    
+function editProduct (id) { 
+    btnUpdate.setAttribute('element', id)
+    let productFound =  inventary.find((product) => product.codigo === id)
+    inputCodigo.value = productFound.codigo
+    inputProducto.value = productFound.producto
+    inputPrecio.value = productFound.precio
+    inputStock.value = productFound.stock
 }
 
-/* let dataCodigo = localStorage.setItem("inputCodigo", inputCodigo) */
+fillTable()
+
+function updateData (id) {
+    inventary.forEach((product) => {
+        if(product.codigo === id) {
+            product.producto = inputProducto.value
+            product.precio = inputPrecio.value
+            product.stock = inputStock.value
+            product.codigo = inputCodigo.value
+        }
+    })
+    deleteProduct()
+    form.reset()
+    addProduct()
+    fillTable()
+} 
+
+
+
+
+
+
+
+
 
